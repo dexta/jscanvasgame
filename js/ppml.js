b87a8d91acc5ad91cfa70c752f24955ace43cdf2
dexta <dexta.de@gmail.com>
2014-02-10 21:55:38 +0100

    just test some standards

--- js/ppml.js
+++ js/ppml.js
@@ -1,9 +1,11 @@
 var KM = {player1UP:87,player1DOWN:83,player2UP:38,player2DOWN:40};
-
+var bp = {x:42,y:42};
 
 function init() {
 
 	playSwitch();
+	bp.x = WIDTH/4;
+	bp.y = HEIGHT/4;
 	}
 	
 function draw() {
@@ -13,8 +15,24 @@ function draw() {
   	lastUpdate = now;
 	animationID = requestAnimationFrame(draw);
 	
+	if(keys.up) bp.y -=3;
+
 	clearCTX();
 	
+	ctx.fillStyle = "rgb(0,0,155)";
+	ctx.strokeStyle = "rgb(23,44,233)";
+	
+	ctx.fillRect(WIDTH/2,0,123,HEIGHT/3);
+	ctx.fillRect(WIDTH/2,HEIGHT/2,123,HEIGHT/2);
+	ctx.stroke();
+	ctx.fill();
+
+
+	ctx.fillStyle = "rgb(142,0,23)";
+	ctx.fillRect(bp.x,bp.y++,42,42)
+
+	ctx.stroke();
+	ctx.fill();
 	
 	var iFPS = parseInt(fps);
 	$("#middle").html(""+iFPS);
